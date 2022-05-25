82ee4c7a9865e8c87c5c1d402309118e39b8f638
mallick-portfolio <tamal@gmail.com>
2022-05-25 12:37:02 +0600

    adder admin role

--- index.js
+++ index.js
@@ -183,6 +183,12 @@ async function run() {
     });
 
     /* =========Admin Section======== */
+    app.get('/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin})
+    })
 
     app.put("/user/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
@@ -200,6 +206,9 @@ async function run() {
         res.send(result);
       }
     });
+
+
+
 
     /* ====== End Admin Section====== */
   } finally {
