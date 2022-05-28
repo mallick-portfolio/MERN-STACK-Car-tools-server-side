94e879b7f446d9479b878dec7ee847acbfdc999a
mallick-portfolio <tamal@gmail.com>
2022-05-28 12:17:44 +0600

    update cors policy

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@cluster0.ho7bb.mongodb.net/?retryWrites=true&w=majority`;
